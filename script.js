ee0d52a9c786802942a7b1269223e49a4f7763a1
Jose Aguirre <aguirrejose615@gmail.com>
2023-10-23 16:12:23 -0400

    dropwdown bug fixed

--- script.js
+++ script.js
@@ -24,30 +24,34 @@ $(".hamburger-menu").click(function() {
     $("#mobile-nav").slideToggle("slow");
 });
 
-  // Script for dropdown on click
-$('.dropdown > a').on('click', function(e) {
-      e.preventDefault(); // Prevent the default action of the anchor tag
-    var dropdownContent = $(this).next('.dropdown-content');
-    dropdownContent.slideToggle("slow");
+    // Script for dropdown on click
+    $('.dropdown > a').on('click', function(e) {
+        e.preventDefault(); // Prevent the default action of the anchor tag
 
-      // Toggle "active" class for the clicked dropdown
-    $(this).parent('.dropdown').toggleClass('active');
+        // Only proceed if the clicked element doesn't have a parent dropdown
+        if ($(this).parents('.dropdown').length === 1) {
+            var dropdownContent = $(this).next('.dropdown-content');
+            dropdownContent.slideToggle("slow");
 
-      // Close other dropdowns and remove their "active" class
-    $('.dropdown').not($(this).parent('.dropdown')).find('.dropdown-content').slideUp("slow");
-    $('.dropdown').not($(this).parent('.dropdown')).removeClass('active');
-});
+            // Toggle "active" class for the clicked dropdown
+            $(this).parent('.dropdown').toggleClass('active');
 
-  // Close the dropdown when clicking outside it
-$(document).on('click', function(e) {
-    if (!$(e.target).closest('.dropdown').length) {
-        $('.dropdown-content').slideUp("slow");
-          $('.dropdown').removeClass('active'); // Remove the "active" class when clicking outside
-    }
-});
+            // Close other dropdowns and remove their "active" class
+            $('.dropdown').not($(this).parent('.dropdown')).find('.dropdown-content').slideUp("slow");
+            $('.dropdown').not($(this).parent('.dropdown')).removeClass('active');
+        }
+    });
 
-  // Prevent dropdown menu from closing when clicking on it
-$('.dropdown-content').click(function(e) {
-    e.stopPropagation();
-});
+    // Separate handler for nested dropdowns
+    $('.dropdown .dropdown .dropdown-toggle').on('click', function(e) {
+        e.stopPropagation(); // Prevent this event from bubbling up to the parent dropdown
+
+        // Your code for handling nested dropdowns here
+        var dropdownContent = $(this).next('.dropdown-content');
+        dropdownContent.slideToggle("slow");
+        
+        // Toggle "active" class for the clicked dropdown
+        $(this).parent('.dropdown').toggleClass('active');
+    });
+
 });
